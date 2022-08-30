58a1d7e97ab108ac7ca1449aee09ee1c721e98a6
CJMontiel <joaquin.montiel@tufesa.com.mx>
2022-08-29 17:40:29 -0700

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { reduce } from 'rxjs';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+import { products } from './product.model';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+
+
+  
+  imgParent ='https://www.w3schools.com/howto/img_avatar.png';
+  register = {
+    name: "",
+    email: "",
+    password: ""
+  }
+
+
+  box = {
+    width:100,
+    height: 100,
+    background: 'red'
+  }
+
+  widthImg = 100;
+  name = 'Nicolas';
+  age = 18
+  img = 'https://source.unsplash.com/random'
+  btnDisabled = true;
+  
+  person={
+    name: 'Carlos',
+    age: 22,
+    avatar: 'https://source.unsplash.com/random'
+  }
+  
+
+  names: string[] = ['Nico', 'Juli', 'Samm', 'Eduardo']
+  newName= ''
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled
+  }
+  ageIncrease(){
+    this.person.age ++
+  }
+  OnScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name= element.value;
+  }
+
+  addName(){
+    this.names.push(this.newName);
+  }
+  onRegister(){
+    console.log(this.register);
+  }
+  onLoaded(img:string){
+    console.log('log padre', img);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { ChangeVocalsToNumbersPipe } from './pipes/change-vocals-to-numbers.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent,
+    ProductComponent,
+    ProductsComponent,
+    NavComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    ChangeVocalsToNumbersPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
